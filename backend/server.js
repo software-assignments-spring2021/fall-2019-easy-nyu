cc9320939aeda2ab80c9ee9632bad53f171091e4
SkipperLin <xl1777@nyu.edu>
2019-10-14 01:12:10 -0400

    Fix a printed string

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database is connected successfully");
 })
 
 app.listen(port, () => {
