1cc1f0000c911fdf91142304a9b5b410f23f4d76
SkipperLin <xl1777@nyu.edu>
2019-10-15 00:08:40 -0400

    Added a few funcs for course object

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 let Course = require('../models/course.model');
 
+// Get Request
 router.route('/').get((req, res) => {
     Course.find()
         .then(courses => res.json(courses))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// Post Request - Add a new course to database
 router.route('/add').post((req, res) => {
     const coursename = req.body.coursename;
     const description = req.body.description;
@@ -24,5 +26,37 @@ router.route('/add').post((req, res) => {
         .then(() => res.json('Course added!'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
+
+// Get course by id
+router.route('/:id').get((req,res) => {
+    Course.findById(req.params.id)
+        .then(course => res.json(course))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Delete course by id
+router.route('/:id').delete((req,res) => {
+    Course.findByIdAndDelete(req.params.id)
+        .then(course => res.json('Course deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Update course by id
+router.route('/update/:id').post((req,res) => {
+    Course.findById(req.params.id)
+        .then(course => {
+            course.coursename = req.body.coursename;
+            course.description = req.body.description;
+            course.semester = req.body.semester;
+            course.prof = req.body.prof;
+            course.ta = req.body.ta;
+
+            course.save()
+                .then(() => res.json('Course updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 
 module.exports = router;
