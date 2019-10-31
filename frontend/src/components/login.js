6dac4ef23a99325acb1ce8e0a900aa0c13189806
hzzyy1997 <hzzyy1997@gmail.com>
2019-10-31 16:54:47 -0400

    fixe signup frontend to have error msg

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,30 +1,101 @@
-import React from "react";
-import Popup from "reactjs-popup";
+import React, { Component } from "react";
+import Modal from 'react-bootstrap/Modal'
 import './login.css'
-export default () => (
-    <Popup trigger={<button className="buttonLink"> Login </button>} modal>
-        {close => (
-            <div className="modal">
-                <a className="close" onClick={close}>
-                    &times;
-                </a>
-                <div className="header"> Login </div>
-                <form>
-                    <label className="ilabel">
-                        Email:
-                        <input type="text" name="email" />
-                    </label>
-                    <label className="ilabel">
-                        Net ID:
-                        <input type="text" name="nid" />
-                    </label>
-                    <label className="ilabel">
-                        Password:
-                        <input type="text" name="password" />
-                    </label>
-                    <button className="login-btn">Login</button>
-                </form>
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            email: "",
+            nid: "",
+            password: "",
+            password2: "",
+            showModal: false,
+            errorMsg: ""
+        };
+    }
+
+    handlePopup = () => {
+        this.setState({ showModal: true });
+    }
+
+    handleEmailChange = (event) => {
+        this.setState({ email: event.target.value });
+    }
+
+    handleNidChange = (event) => {
+        this.setState({ nid: event.target.value });
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({ password: event.target.value });
+    }
+
+    handleClose = (event) => {
+        this.setState({ showModal: false });
+    }
+
+    send() {
+        fetch('/api/users/login', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                nid: this.state.nid,
+                password: this.state.password,
+                password2: this.state.password2
+            })
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    response.json()
+                    .then(errors => {
+                        this.setState({
+                            errorMsg: Object.values(errors)[0]
+                        })
+                    })
+                }
+            })
+            .then(
+                this.setState({
+                    nid: "",
+                    password: "",
+                })
+            );
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="buttonLink" onClick={this.handlePopup}> Login </button>
+                <Modal show={this.state.showModal} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Login</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <label className="ilabel">
+                            Net ID:
+                            <input type="text" name="nid" value={this.state.nid} onChange={this.handleNidChange} />
+                        </label>
+                        <label className="ilabel">
+                            Password:
+                            <input type="text" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                        </label>
+                        <p className='error-msg'>{this.state.errorMsg}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button className="login-btn" onClick={(evt) => { this.send(); }}>Login</button>
+                    </Modal.Footer>
+                </Modal>
             </div>
-        )}
-    </Popup>
-);
+        )
+    }
+}
+
+export default Login;
--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,38 +1,122 @@
-import React from "react";
-import Popup from "reactjs-popup";
+import React, { Component } from "react";
+import Modal from 'react-bootstrap/Modal'
 import './signup.css'
-export default () => (
-    <Popup trigger={<button className="buttonLink"> Create Account </button>} modal>
-        {close => (
-            <div className="modal">
-                <a className="close" onClick={close}>
-                    &times;
-                </a>
-                <div className="header"> Create Account </div>
-                <form>
-                    <label className="ilabel">
-                        Name:
-                        <input type="text" name="name" />
-                    </label>
-                    <label className="ilabel">
-                        Email:
-                        <input type="text" name="email" />
-                    </label>
-                    <label className="ilabel">
-                        Net ID:
-                        <input type="text" name="nid" />
-                    </label>
-                    <label className="ilabel">
-                        Password:
-                        <input type="text" name="password" />
-                    </label>
-                    <label className="ilabel">
-                        Confirm Password:
-                        <input type="text" name="password2" />
-                    </label>
-                    <button className="signup-btn">Create Account</button>
-                </form>
+
+class Signup extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        name: "",
+        email: "",
+        nid: "",
+        password: "",
+        password2: "",
+        showModal: false,
+        errorMsg: ""
+      };
+    }
+    
+    handlePopup = () => {
+        this.setState({showModal : true});
+    }
+
+    handleNameChange = (event) => {
+      this.setState({ name: event.target.value });
+    }
+
+    handleEmailChange = (event) => {
+        this.setState({ email: event.target.value });
+    }
+
+    handleNidChange = (event) => {
+        this.setState({ nid: event.target.value });
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({ password: event.target.value });
+    }
+
+    handlePassword2Change = (event) => {
+        this.setState({ password2: event.target.value });
+    }
+
+    handleClose = (event) => {
+        this.setState({ showModal: false });
+    }
+
+    send() {
+        fetch('/api/users/register', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                nid: this.state.nid,
+                password: this.state.password,
+                password2: this.state.password2
+            })
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    response.json()
+                    .then(errors => {
+                        this.setState({
+                            errorMsg: Object.values(errors)[0]
+                        })
+                    })
+                }
+            }).then(
+                this.setState({
+                    name: "",
+                    email: "",
+                    nid: "",
+                    password: "",
+                    password2: "",
+                })
+            );
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="buttonLink" onClick={this.handlePopup}> Create Account </button>
+                <Modal show={this.state.showModal} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Create Account</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <label className="ilabel">
+                            Name:
+                            <input type="text" name="name" value={this.state.name} onChange={this.handleNameChange} />
+                        </label>
+                        <label className="ilabel">
+                            Email:
+                            <input type="text" name="email" value={this.state.email} onChange={this.handleEmailChange} />
+                        </label>
+                        <label className="ilabel">
+                            Net ID:
+                            <input type="text" name="nid" value={this.state.nid} onChange={this.handleNidChange} />
+                        </label>
+                        <label className="ilabel">
+                            Password:
+                            <input type="text" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                        </label>
+                        <label className="ilabel">
+                            Confirm Password:
+                            <input type="text" name="password2" value={this.state.password2} onChange={this.handlePassword2Change} />
+                        </label>
+                        <p className='error-msg'>{this.state.errorMsg}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button className="signup-btn" onClick={(evt) => { this.send(); }}>Create Account</button>
+                    </Modal.Footer>
+                </Modal>
             </div>
-        )}
-    </Popup>
-);
+        )
+    }
+}
+  
+export default Signup;
