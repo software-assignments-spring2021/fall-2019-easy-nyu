6b4f6e44376a2b46b439f9467d0b62742b209d2b
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2019-11-03 18:18:16 -0500

    Create courseSearch.js

---
+++ frontend/src/components/courseSearch.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+
+class CourseSearch extends Component {
+    constructor(props) {
+        super(props);
+        this._isMounted = false;
+        this.state = {
+            courses: [],
+        };
+    }
+
+    componentWillMount() {
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+    
+    componentDidMount() {
+        fetch('/courses/', { method: "GET" })
+            .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Network response was not ok.');
+            }
+            }).then(response => {
+            if (this._isMounted) {
+                console.log(response)
+                this.setState({ courses: response })
+            }
+        });
+    }
+    render () {
+        return (
+				<div>
+					<div id="header" class="headerContent">
+						<img class="headerLogo" src="logo.png">
+						</img>
+					</div>
+
+					<div id="title" class="pageTitle">EasyNYU Course Search</div>
+						<div class="centerContent">
+							<div class="textBoxContainer shadow rounded">
+							<table style={{margin: "5px auto"}}><thead>
+								<tr>
+									<th>Course</th>
+									<th>Professor</th>
+								</tr>
+							</thead><tbody>
+								{this.state.courses.map((course, i) => (
+									<tr>
+										<td><Link>{course.coursename}</Link></td>
+										<td><Link>{course.prof}</Link></td>
+									</tr>
+								))}
+							</tbody></table>
+						</div>
+					</div>
+				</div>
+        )
+    }
+}
+  
+export default CourseSearch;
