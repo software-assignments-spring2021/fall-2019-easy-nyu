4e785c3f84983d4cea11065d8052e0f4df529724
SkipperLin <xl1777@nyu.edu>
2019-11-04 17:04:18 -0500

    Implement comment to a prof in general

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Comment = require('../models/comment.model');
 const Course = require('../models/course.model');
+const Prof = require('../models/professor.model');
 
 // Post Request - Add a new course to database
 router.route('/add').post((req, res) => {
@@ -35,6 +36,23 @@ router.route('/add').post((req, res) => {
                     })
                 .catch(err => res.status(400).json('Error: ' + err));
                 res.json({message: "Comment added!", course_id: data.course_id});
+            }
+        })
+    } else if (!course_id) {
+        newComment = new Comment({'comment' : user_comment, 'prof_id' : prof_id});
+        newComment.save((err, data) => {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                Prof.findById(prof_id)
+                    .then(prof => {
+                        prof.comments.push(data._id);
+                        prof.save()
+                            .catch(err => res.status(400).json('Error: ' + err));
+                    })
+                .catch(err => res.status(400).json('Error: ' + err));
+                res.json({message: "Comment added!", prof_id: data.prof_id});
             }
         })
     }
