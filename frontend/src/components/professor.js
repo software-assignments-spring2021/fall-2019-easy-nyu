87326b0416355a22b2d3bb5e0e8350277f1b4bb4
Xuanzheng Lin <xl1777@nyu.edu>
2019-11-07 19:08:10 -0500

    Revert "Integrate professor display into course search"

--- frontend/src/components/professor.js
+++ frontend/src/components/professor.js
@@ -9,10 +9,13 @@ class Professor extends Component {
 
     render () {
         return (
-            <tr>
-                <td><Link to={"/coursesearch/professors/" + this.props.id}>{this.props.name}</Link></td>
-                <td>{this.props.description}</td>
-            </tr>
+            <div id="show-prof-div" style={{ textAlign: "center" }}>
+                <p>
+                    Prof Name: {this.props.name} <br />
+                    Description: {this.props.description} <br />
+                    <br />
+                </p>
+            </div>
         )
     }
 }
