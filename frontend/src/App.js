fb4731912f3da79115ddcc8e577e08581ca035c7
Matthew Stillman <mstillman9@verizon.net>
2019-11-08 18:51:02 -0500

    revert commit b93235ee2da4807e47bbecfdfa3a66eab47de84f

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import './App.css';
 import Login from './components/login'
 import Signup from './components/signup'
+import ProfessorDisplay from './components/professorDisplay';
 
 function App() {
 	return (
@@ -19,8 +20,7 @@ function App() {
 					<div class="textBox">
 						<Login /><br />
 						<Signup /><br />
-						<Link to='/coursesearch' className="buttonLink">Public Course Search</Link><br />
-						<Link to='/professorsearch' className="buttonLink">Public Professor Search</Link>
+						<Link to='/coursesearch' className="buttonLink">Public Course Search</Link>
 					</div>
 				</div>
 			</div>
