c770974393b943d5015ad69070ad0c565c8bc4cc
SkipperLin <xl1777@nyu.edu>
2019-11-09 01:33:38 -0500

    Missed one coursearch occurance, but fixed now

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import App from './App';
-import CourseSearch from './components/courseSearch';
+import Portal from './components/portal';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Router>
-<Route exact path="/" component={App} >
-</Route>
-<Route path="/coursesearch" component = {CourseSearch}>
-</Route>
-</Router>, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<Router>
+        <Route exact path="/" component={App} ></Route>
+        <Route path="/portal" component = {Portal}></Route>
+        </Router>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
