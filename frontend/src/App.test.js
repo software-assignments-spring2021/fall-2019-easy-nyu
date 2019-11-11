f6fdee5cf4b5be34a89ad54d1119ca817b933524
Yiyun Zhang <hzzyy1997@gmail.com>
2019-11-10 22:45:11 -0500

    corected frontend test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import App from './App';
-import Portal from './components/portal';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Router>
-        <Route exact path="/" component={App} ></Route>
-        <Route path="/portal" component = {Portal}></Route>
-        </Router>, div);
+    ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
