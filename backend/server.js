b37981a644753a74813faca320550085a5ce774d
Xuanzheng Lin <xl1777@nyu.edu>
2019-11-12 16:24:39 -0500

    Revert "User story/109/task/125/backend profile"

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,6 @@ function connect_to_db () {
   app.use('/comments', commentsRouter);
   const professorRouter = require('./routes/professors');
   app.use('/professors', professorRouter);
-  const userRouter = require('./routes/user');
-  app.use('/users', userRouter);
 
   // Passport middleware
   app.use(passport.initialize());
