1b5e95e92c24f62f539774775f0e0ee680dcf5b2
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2019-11-18 23:48:12 -0500

    let's try this

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { shallow, mount, render } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders without crashing', () => {
+    var wrapper = mount(<App />);
 });
