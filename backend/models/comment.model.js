9acd62fa53110c657a8ddbce3e2dcffcf1b94951
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2019-12-05 20:04:36 -0500

    Update comment schema

--- backend/models/comment.model.js
+++ backend/models/comment.model.js
@@ -12,7 +12,9 @@ const commentSchema = new Schema({
     },
     course_id: { type : mongoose.Schema.Types.ObjectId, ref: 'Course' },
     prof_id: { type : mongoose.Schema.Types.ObjectId, ref: 'Professor' },
-    comment_id: mongoose.Schema.ObjectId
+    comment_id: mongoose.Schema.ObjectId,
+	user_id: { type : mongoose.Schema.Types.ObjectId, ref: 'User' },
+	anonymous: Boolean
 }, {
     timestamps: true,
 });
