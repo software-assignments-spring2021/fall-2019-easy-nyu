277052fc56048348203506a635fd3d53e2fbc258
Kailin Zheng <kz882@nyu.edu>
2019-12-06 13:16:55 -0500

    initial-setup

--- frontend/src/components/userProfile.js
+++ frontend/src/components/userProfile.js
@@ -40,10 +40,86 @@ class UserProfile extends Component {
 
     render() {
         return (
-            <div>
-                <Navbar />
-                show whatever please
-            </div>
+            return (
+                <div>
+                    <NYUNavBar />
+                    <center><container>
+                        <Row className="justify-content-md-center">
+                            <h1>N-id: {this.state.nid}</h1>
+                        </Row>
+                        <Row className="justify-content-md-center">
+                            <h3>Score: {this.state.scorel}</h3>
+                        </Row>
+                        <Row className="justify-content-md-center">
+                            <AddComment profid={this.props.match.params.id}/>
+                        </Row>
+                        <Table striped bordered hover>
+                            <tr>
+                                <th>Comments for the Professor</th>
+                            </tr>
+                            <tr>
+                                {this.state.comments_for_prof.map((prof_comment, i) => (
+                                    <td key={i}>
+                                        <p>{prof_comment}</p>
+                                    </td>
+                                ))}
+                            </tr>
+                        </Table>
+    
+                        {/* {Option One for Display} */}
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>{`Courses Taught By ${this.state.professorname}`}</th>
+                                    <th>{`Comments on Course`}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.courses.map((course, i) => (
+                                    <tr key={course._id}>
+                                            <td>
+                                                <Link to={`/course/${course._id}`}>{course.name}</Link>
+                                            </td>
+                                            <td>
+                                            {this.state.comments[i].map((comment, j) => (
+                                                <p key={j}>{this.state.comments[i][j]}</p>
+                                            ))}
+                                            </td>
+                                    </tr>
+                                    ))}
+                            </tbody>
+                        </Table>
+                        
+                        {/* {Option Two for Display} */}
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>{`Courses Taught By ${this.state.professorname}`}</th>
+                                    <th>{`Comments on Course`}</th>
+                                </tr>
+                            </thead>
+                                {this.state.courses.map((course, i) => (
+                                    <tr>
+                                        <td key={course._id}>
+                                            <Link to={`/course/${course._id}`}>{course.name}</Link>
+                                        </td>
+                                        <td>
+                                            <table>
+                                                <tr>
+                                                    {this.state.comments[i].map((comment, j) => (
+                                                        <td key={j}>
+                                                            <p>{this.state.comments[i][j]}</p>
+                                                        </td>
+                                                    ))}
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                ))}
+                        </Table>
+                    </container></center>
+                </div>
+            )
         )
     }
 }
