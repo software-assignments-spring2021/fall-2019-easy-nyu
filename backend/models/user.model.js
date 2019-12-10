5bbe9a1bc12eae13b927e6756686874e274996be
Kailin Zheng <kz882@nyu.edu>
2019-12-09 22:54:29 -0500

    edited-user-model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,6 +6,8 @@ const userSchema = new Schema({
     nid: { type: String, required: true },
     description: { type: String, required: true },
     score: { type: Number, required: true}
+    starprof: [{ type : mongoose.Schema.Types.ObjectId, ref: 'Professor' }],
+    starcourse: [{ type : mongoose.Schema.Types.ObjectId, ref: 'Course' }]
 }, {
     timestamps: true,
 });
