1efaa02cd9339903f985c9c253812b6ee2f38194
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2019-12-14 01:44:38 -0500

    fix grammar, css

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -18,7 +18,7 @@ class Home extends Component {
                 <div class="centerContent">
                     <div class="textBoxContainer shadow rounded">
                         <div class="textBox">
-                            <p><font size="+1"> 
+                            <div style={{'width':'300px'}}><p><font size="+1"> 
                                 EasyNYU is a web-based platform 
                                 that allows NYU students to gain a better 
                                 insight before selecting a class and to 
@@ -26,17 +26,17 @@ class Home extends Component {
                                 schoolmates.
                             </font></p>
                             <p><font size="+1"> 
-                                EasyNYU Team is dedicated to creating 
-                                a friendly and safe community for students 
+                                The EasyNYU Team is dedicated to creating 
+                                a safe and friendly community for students 
                                 to freely comment on 
                                 the courses taken and on 
                                 associated professors. 
                             </font></p>
                             <p><font size="+1"> 
-                                Start by search your favorite course or professor
-                                and share your opinions! You will need to login before
+                                Start by searching for your favorite courses and professors
+                                and sharing your opinion! You will need to login before
                                 adding a comment.
-                            </font></p>
+                            </font></p></div>
                             <center><Login buttonLocation={"homepage"}/></center>
                         </div>
                     </div>
