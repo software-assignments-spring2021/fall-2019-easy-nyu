d3037ff771f9134876df09cedbe09c69441f60b9
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2019-12-16 15:41:04 -0500

    test mongodb server

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ function connect_to_db() {
     // Connect to MongoDB
     let uri = process.env.ATLAS_URI;
     if (process.env.NODE_ENV === "test") {
-        uri = process.env.Testing_URI;
+        uri = "mongodb+srv://Jack_Zhang:zyy1997@cluster0-ktrme.mongodb.net/new_eznyu?retryWrites=true&w=majority";
     }
     console.log(uri)
     mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
