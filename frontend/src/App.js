7db6553c1f85e58f4a9a2bd504c6d07b63530970
Yiyun Zhang <hzzyy1997@gmail.com>
2019-12-15 23:38:38 -0500

    deleted old home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
-import CourseDisplay from './components/courseDisplay';
-import ProfessorDisplay from './components/professorDisplay'
 import Home from './components/home';
 import CourseDetail from './components/courseDetail'
 import ProfessorProfile from './components/professorProfile'
@@ -11,8 +9,6 @@ import UserProfile from './components/userProfile'
 function App() {
 	return (
 		<Router>
-			<Route exact path="/course" component={CourseDisplay}></Route>
-			<Route exact path="/professor" component={ProfessorDisplay}></Route>
 			<Route exact path="/" component={Home}></Route>
 			<Route exact path="/course/:id" component={CourseDetail} />
 			<Route exact path="/professor/:id" component={ProfessorProfile} />
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-import './home.css';
-import Login from './login'
-import Signup from './signup'
 import Navbar from './navbar'
+import {Grid, Paper, makeStyles, Button} from '@material-ui/core';
 
 class Home extends Component {
     constructor(props) {
@@ -11,38 +9,16 @@ class Home extends Component {
     }
 
     render() {
+        let { classes } = this.props;
+        console.log(classes)
         return (
             <div>
                 <Navbar />
-                <div id="title" className="pageTitle">Welcome to EasyNYU</div>
-                <div class="centerContent">
-                    <div class="textBoxContainer shadow rounded">
-                        <div class="textBox">
-                            <p><font size="+1"> 
-                                EasyNYU is a web-based platform 
-                                that allows NYU students to gain a better 
-                                insight before selecting a class and to 
-                                share information with his or her fellow 
-                                schoolmates.
-                            </font></p>
-                            <p><font size="+1"> 
-                                The EasyNYU Team is dedicated to creating 
-                                a safe and friendly community for students 
-                                to freely comment on 
-                                the courses taken and on 
-                                associated professors. 
-                            </font></p>
-                            <p><font size="+1"> 
-                                Start by searching for your favorite courses and professors
-                                and sharing your opinion! You will need to login before
-                                adding a comment.
-                            </font></p>
-                        </div>
-                        <div class="textBox">
-                            <Login buttonLocation={"homepage"}/>
-                        </div>
-                    </div>
-                </div>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Button variant="outlined" color="primary">Search by Major</Button>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
