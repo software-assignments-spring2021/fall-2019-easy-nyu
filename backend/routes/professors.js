d19ae1e43672f5dde662b1b1d23c1fe9374bdf02
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2019-12-16 21:26:22 -0500

    add jwt token to add professor

--- backend/routes/professors.js
+++ backend/routes/professors.js
@@ -1,26 +1,46 @@
 const router = require('express').Router();
 let Professor = require('../models/professor.model');
+const jwt = require("jsonwebtoken");
 
 // create professor 
 router.route('/add').post((req, res) => {
-    const name = req.body.name;
-    const courses = req.body.courses;
-    const comments = req.body.comments;
-    const newProfessor = new Professor({
-        name,
-        courses,
-        comments
-    });
+    var admin = false;
+	var token = req.headers['authorization'];
+	if (token) {
+		token = token.slice(7, token.length);
+	}
+	jwt.verify(token, process.env.secretOrKey, (err, decoded) => {
+		if (err) {
+			admin = false;
+		} else {
+			admin = (decoded.role == "admin");
+        }
+        
+        if (admin) {
+            const name = req.body.name;
+            const school = req.body.school;
+            const courses = req.body.courses;
+            const comments = req.body.comments;
+            const newProfessor = new Professor({
+                name,
+                school,
+                courses,
+                comments
+            });
 
-    newProfessor.save((err, prof) => {
-        if (err) {
-            res.send(err);
+            newProfessor.save((err, prof) => {
+                if (err) {
+                    res.send(err);
+                }
+                else {
+                    console.log("Professor added!", prof.name)
+                    res.json({message: "Professor added!", prof: prof});
+                }
+            })
+        } else {
+			res.status(401).json({unauthorized: '401 Unauthorized'});
         }
-        else {
-            console.log("Professor added!", prof.name)
-            res.json({message: "Professor added!", prof: prof});
-        }
-    })
+    });
 });
 
 // Get All Professor
