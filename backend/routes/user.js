ea260d9ae2f6f7ddba86e2f449f263fee38afaca
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2019-12-18 01:05:12 -0500

    re-add comments route to user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const Auth = require("../models/auth.model");
+const Comments = require("../models/comment.model");
+const jwt = require("jsonwebtoken");
 
 // Find user by net id
 router.route('/:nid').get((req, res) => {
@@ -27,6 +29,41 @@ router.route('/update').post((req,res) => {
                     .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/comments/:nid').get((req, res) => {
+	var authorized = false;
+	var admin = false;
+	var requesterNid;
+	var token = req.headers['authorization'];
+	if (token) {
+		token = token.slice(7, token.length);
+	}
+	jwt.verify(token, process.env.secretOrKey, (err, decoded) => {
+		if (err) {
+			authorized = false;
+		} else {
+			authorized = true;
+			requesterNid = decoded.id;
+			admin = (decoded.role == "admin")
+        }
+
+        var query;
+
+        if (requesterNid == req.params.nid || admin) {
+            query = {user_id: req.params.nid};
+        } else {
+            query = {user_id: req.params.nid, anonymous: false};
+        }
+
+        Comments.find(query).populate("prof_id").populate("course_id").sort({createdAt: 1}).exec((err, data) => {                
+            if (err) {
+                res.status(500).json('Error: ' + err)
+            } else {
+                res.json(data);
+            }
+        });
+    });
 });
 
 module.exports = router;
